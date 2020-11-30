81cde06d1de6f98cd64bfc4804b426f24c97afbe
ThaysMrtns <thays.martins@nave.org.br>
2020-11-30 10:43:15 -0300

    Public page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations'}
 
-  root to: "funcionarios#index";
+  root to: "public#index";
   resources :funcionarios, only:[:index, :show, :new, :create, :update, :destroy];
 end

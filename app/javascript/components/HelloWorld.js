a99e30491b3812f8af4bc2f7c44e147c84645c4f
Diego Souza <diego@livima.com.br>
2020-12-01 13:03:48 -0300

    Implementação do Ant Design

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,10 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Button, DatePicker } from 'antd';
+import 'antd/dist/antd.css';
+
 class HelloWorld extends React.Component {
   render () {
     return (
       <React.Fragment>
         Texto: {this.props.greeting}
+        <Button type="primary">PRESS ME</Button>
       </React.Fragment>
     );
   }

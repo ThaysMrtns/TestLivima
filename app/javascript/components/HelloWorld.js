006f77a9e63b4152e629b94d1f4d81c051756086
Diego Souza <diego@livima.com.br>
2020-12-01 13:18:47 -0300

    Implementação de header

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,14 +1,25 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { Button, DatePicker } from 'antd';
+import {PageHeader, Button, Descriptions} from 'antd';
 import 'antd/dist/antd.css';
 
 class HelloWorld extends React.Component {
   render () {
     return (
       <React.Fragment>
-        Texto: {this.props.greeting}
-        <Button type="primary">PRESS ME</Button>
+          <PageHeader
+            ghost={false}
+            title="My Team"
+            subTitle="Gerencie seu time"
+            extra={[
+              <Button key="3">Sobre</Button>,
+              <Button key="2">Cadastrar</Button>,
+              <Button key="1" type="primary">
+                Entrar
+              </Button>,
+            ]}
+          >
+          </PageHeader>
       </React.Fragment>
     );
   }

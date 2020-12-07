1e15eb1c2562b3fd075da04f7d69e3ca9f01ca18
ThaysMrtns <thays.martins@nave.org.br>
2020-12-07 17:52:24 -0300

    Form

--- app/javascript/components/AddPersonTeamForm.js
+++ app/javascript/components/AddPersonTeamForm.js
@@ -1,9 +1,29 @@
 import PropTypes from "prop-types";
 import React, { useState } from 'react';
-import { Modal, Button } from 'antd';
+import { Modal, Button, Form, Input, Checkbox} from 'antd';
 
 function AddPersonTeamForm(){
+  //Modal
   const [visible, setVisible] = useState(false);
+
+  //Form
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
+  const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+  };
+  
+  const Demo = () => {
+    const onFinish = values => {
+      console.log('Success:', values);
+    };
+  
+    const onFinishFailed = errorInfo => {
+      console.log('Failed:', errorInfo);
+    };
+  
     return (
       <React.Fragment>
         <>
@@ -18,9 +38,15 @@ function AddPersonTeamForm(){
           onCancel={() => setVisible(false)}
           width={1000}
         >
-          <p>some contents...</p>
-          <p>some contents...</p>
-          <p>some contents...</p>
+          <Form>
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: 'Please input your username!' }]}
+          >
+            <Input />
+          </Form.Item>
+          </Form>
         </Modal>
       </>
       </React.Fragment>

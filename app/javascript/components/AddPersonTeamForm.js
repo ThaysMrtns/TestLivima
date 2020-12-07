1f1545eff979fb9d25847fe19f9985bdb447516f
ThaysMrtns <thays.martins@nave.org.br>
2020-12-07 17:24:24 -0300

    Mudando de componente de classe para funcional

---
+++ app/javascript/components/AddPersonTeamForm.js
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+import React, { useState } from 'react';
+import { Modal, Button } from 'antd';
+//extends React.Component
+function AddPersonTeamForm(){
+  
+    return (
+      <React.Fragment>
+        <Button>oi</Button>
+      </React.Fragment>
+    );
+  
+}
+
+export default AddPersonTeamForm

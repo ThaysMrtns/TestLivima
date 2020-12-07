0f921429e75ef538b55484826f1b7302953c363f
ThaysMrtns <thays.martins@nave.org.br>
2020-12-07 17:30:43 -0300

    Detalhes

--- app/javascript/components/AddPersonTeamForm.js
+++ app/javascript/components/AddPersonTeamForm.js
@@ -1,12 +1,28 @@
 import PropTypes from "prop-types";
 import React, { useState } from 'react';
 import { Modal, Button } from 'antd';
-//extends React.Component
+
 function AddPersonTeamForm(){
-  
+  const [visible, setVisible] = useState(false);
     return (
       <React.Fragment>
-        <Button>oi</Button>
+        <>
+        <Button type="primary" onClick={() => setVisible(true)}>
+          Adicionar pessoas ao time
+        </Button>
+        <Modal
+          title="Adicionar pessoas"
+          centered
+          visible={visible}
+          onOk={() => setVisible(false)}
+          onCancel={() => setVisible(false)}
+          width={1000}
+        >
+          <p>some contents...</p>
+          <p>some contents...</p>
+          <p>some contents...</p>
+        </Modal>
+      </>
       </React.Fragment>
     );
   

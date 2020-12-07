4cf2aad7ee3517ec92e478713dfac297cc563ab5
ThaysMrtns <thays.martins@nave.org.br>
2020-12-07 18:03:54 -0300

    Corrigindo erro

--- app/javascript/components/AddPersonTeamForm.js
+++ app/javascript/components/AddPersonTeamForm.js
@@ -2,56 +2,33 @@ import PropTypes from "prop-types";
 import React, { useState } from 'react';
 import { Modal, Button, Form, Input, Checkbox} from 'antd';
 
-function AddPersonTeamForm(){
+export default function AddPersonTeamForm(){
   //Modal
   const [visible, setVisible] = useState(false);
 
   //Form
-  const layout = {
-    labelCol: { span: 8 },
-    wrapperCol: { span: 16 },
-  };
-  const tailLayout = {
-    wrapperCol: { offset: 8, span: 16 },
-  };
   
-  const Demo = () => {
-    const onFinish = values => {
-      console.log('Success:', values);
-    };
-  
-    const onFinishFailed = errorInfo => {
-      console.log('Failed:', errorInfo);
-    };
   
     return (
       <React.Fragment>
         <>
-        <Button type="primary" onClick={() => setVisible(true)}>
-          Adicionar pessoas ao time
-        </Button>
-        <Modal
-          title="Adicionar pessoas"
-          centered
-          visible={visible}
-          onOk={() => setVisible(false)}
-          onCancel={() => setVisible(false)}
-          width={1000}
-        >
-          <Form>
-          <Form.Item
-            label="Username"
-            name="username"
-            rules={[{ required: true, message: 'Please input your username!' }]}
+          <Button type="primary" onClick={() => setVisible(true)}>
+            Adicionar pessoas ao time
+          </Button>
+          <Modal
+            title="Adicionar pessoas"
+            centered
+            visible={visible}
+            onOk={() => setVisible(false)}
+            onCancel={() => setVisible(false)}
+            width={1000}
           >
-            <Input />
-          </Form.Item>
-          </Form>
-        </Modal>
-      </>
+            <p>some contents...</p>
+            <p>some contents...</p>
+            <p>some contents...</p>
+          </Modal>
+        </>
       </React.Fragment>
     );
   
 }
-
-export default AddPersonTeamForm
